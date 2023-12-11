5d00a8e9ab6bdb8132a0cfe414a01fecf8bcf6a2
jjules <jjules@kakao.com>
2023-12-11 23:10:21 +0900

    init

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -1,16 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("jvm") version "1.9.20"
     kotlin("plugin.spring") version "1.9.20"
-}
-
-group = "com.avante"
-version = "dev"
-java.sourceCompatibility = JavaVersion.VERSION_17
-
-repositories {
-    mavenCentral()
 }
 
 dependencies {
@@ -20,15 +10,4 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
